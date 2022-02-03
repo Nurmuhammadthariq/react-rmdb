8a8a75dd4d4f19da0c3770cbc6c8bbeb98f37c1b
thoriqnoer <thoriq.noer@gmail.com>
2022-02-03 18:09:19 +0700

    Button

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
 import SearchBar from './SearchBar';
+import Button from './Button';
 
 //Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -15,9 +16,14 @@ import { useHomeFetch } from '../hooks/useHomeFetch';
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-  const { state, loading, error, setSearchTerm, searchTerm } = useHomeFetch();
+  const { state, loading, error, setSearchTerm, searchTerm, setIsLoadingMore } =
+    useHomeFetch();
 
   const randomNumber = Math.floor(Math.random() * 10);
+
+  console.log(state);
+
+  //Display error
 
   return (
     <div>
@@ -43,7 +49,10 @@ const Home = () => {
           />
         ))}
       </Grid>
-      {/* <Spinner /> */}
+      {loading && <Spinner />}
+      {state.page < state.total_pages && !loading && (
+        <Button text="Load More" callback={() => setIsLoadingMore(true)} />
+      )}
     </div>
   );
 };
