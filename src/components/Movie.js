6fc6a269017562c95fcc97e26e561e63fa3190ae
thoriqnoer <thoriq.noer@gmail.com>
2022-02-07 03:30:49 +0700

    Page movie

---
+++ src/components/Movie.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+//Components
+import Grid from './Grid';
+import Spinner from './Spinner';
+//Hook
+
+//No Image
+import NoImage from '../images/no_image.jpg';
+
+const Movie = () => {
+  return (
+    <>
+      <h1>This is page movie</h1>
+    </>
+  );
+};
+
+export default Movie;
