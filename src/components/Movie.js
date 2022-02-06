4fcb538cf1478d3170f9ebdef2fe3f655abcd4d2
thoriqnoer <thoriq.noer@gmail.com>
2022-02-07 06:15:27 +0700

    Fetch Movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 // Config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 //Components
+import BreadCrumb from './BreadCrumb';
 import Grid from './Grid';
 import Spinner from './Spinner';
 //Hook
-
+import { useMovieFetch } from '../hooks/useMovieFetch';
 //No Image
 import NoImage from '../images/no_image.jpg';
 
 const Movie = () => {
+  const { movieId } = useParams();
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong...</div>;
+
   return (
     <>
-      <h1>This is page movie</h1>
+      <BreadCrumb movieTitle={movie.original_title} />
     </>
   );
 };
